811cc4b0fceb6ffc03dc24e1eb543e2cf52cd6cc
selebry <murad1703@yandex.ru>
2022-11-09 17:56:22 +0300

    vxcg

--- PasswordValidatorWPFSimple/MainWindow.xaml.cs
+++ PasswordValidatorWPFSimple/MainWindow.xaml.cs
@@ -25,7 +25,34 @@ namespace PasswordValidatorWPFSimple
         {
             InitializeComponent();
             IntegerUpDown integerUpDown = new IntegerUpDown();
-            Stack1.Children.Add(integerUpDown);
+            Stack1.Children.Insert(5,integerUpDown);
+            integerUpDown.Minimum = 6;
+            integerUpDown.Maximum = 255;
+            integerUpDown.Watermark = "Unlimited";
+            integerUpDown.ValueChanged += new RoutedPropertyChangedEventHandler<object>(integerUpDown_ValueChanged);
+            integerUpDown.TextInput += new TextCompositionEventHandler(text_changed);
+           
+            
+
+        }
+
+        private void text_changed(object sender, TextCompositionEventArgs e)
+        {
+            System.Windows.MessageBox.Show("123");
+        }
+
+        private void integerUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            var integerUpDown = Stack1.Children[5] as IntegerUpDown;
+            
+            if(integerUpDown.Value != null)
+            {
+                System.Windows.MessageBox.Show("123");
+            }
+            else
+            {
+                
+            }
         }
     }
 }
